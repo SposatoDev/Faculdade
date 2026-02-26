ec44216aaf620bb03b2a110d9ae7f85302bf989e
Kauã Sato Sposato <131924519+SposatoDev@users.noreply.github.com>
2026-02-26 19:10:34 -0300

    Add Recepcionista class with basic attributes and methods

---
+++ poo/atividade-02/Recepcionista.java
@@ -0,0 +1,21 @@
+import atividade-02;
+
+public class Recepcionista {
+
+  public String nome;
+  public String cpf;
+  public String telefone;
+  public String senha;
+
+  public void acessar () {
+    System.out.println("Sistema acessado! Recepcionista: " + nome);
+  }
+
+  public void mostrar () {
+    System.out.println("Recepcionista");
+    System.out.println("Nome: " + nome);
+    System.out.println("CPF: " + cpf);
+    System.out.println("Telefone: " + telefone);
+  }
+  
+}
