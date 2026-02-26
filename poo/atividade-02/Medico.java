f9d80b3fc0f88076d40f119b44568adb98f4427b
Kauã Sato Sposato <131924519+SposatoDev@users.noreply.github.com>
2026-02-26 18:57:58 -0300

    Add Medico class with basic attributes and methods

---
+++ poo/atividade-02/Medico.java
@@ -0,0 +1,23 @@
+import atividade-02;
+
+public class Medico {
+
+  public String nome;
+  public String crm;
+  public String telefone;
+  public String especialidade;
+  public String senha;
+
+  public void acessar () {
+    System.out.println("Sistema acessado! Médico: " + nome)
+  }
+
+  public void mostrar() {
+    System.out.println("MEDICO");
+    System.out.println("Nome: " + nome);
+    System.out.println("CRM: " + crm);
+    System.out.println("Telefone: " + telefone);
+    System.out.println("Especialidade: " + especialidade);
+  }
+   
+}
